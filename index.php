d3e942d03f61ae2c98afc38e235fea96e2d8dbf5
GtDavide01 <davidefiorini01@gmail.com>
2022-12-08 22:42:14 +0100

    bonus 2 aggiunto

--- index.php
+++ index.php
@@ -16,14 +16,18 @@ Facciamo attenzione all’organizzazione del codice, suddividendolo in appositi 
 require_once __DIR__ . "/models/Movie.php";
 
 $generi = [
-    "Azione",
-    "Avventura",
-    "Fantascienza",
-    "Horror",
+    "Azione ",
+    "Avventura ",
+    "Fantascienza ",
+    "Horror ",
 ];
 
-$movie1 = new Movie("Mission Impossible", $generi, "106 min", "inglese");
-var_dump($movie1);
+$movies = [
+    new Movie("Mission Impossible", $generi, "106 min", "inglese"),
+    new Movie("Creed", $generi, "134 min ", "inglese"),
+    new Movie("Smetto quando voglio ", $generi, "90 min", "italiano "),
+]
+
 
 
 ?>
@@ -35,11 +39,30 @@ var_dump($movie1);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     <title>Php oop 1</title>
 </head>
 
 <body>
-    <h2><?php echo $movie1->getInfoMovie() ?></h2>
+    <h1 class=" mt-5 text-center">PHP oop 1 Film!</h1>
+    <div class="container d-flex mt-5 p-5">
+        <?php foreach ($movies as $movie) { ?>
+            <div class="card m-4" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $movie->title ?></h5>
+                    <h6 class="card-subtitle mb-2 text-muted"><?php echo $movie->lingua ?></h6>
+                    <p class="card-text">
+                        <?php foreach ($movie->genere as $genere) {
+                            echo $genere;
+                        } ?></p>
+                    <p class="card-text"><?php echo $movie->durata ?></p>
+                    <p class="card-text"><?php echo $movie->description ?></p>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
+
 </body>
 
 </html>
